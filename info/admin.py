659e2cab0a63c3105f2946765881d960817666c0
Behnoush <behnoush.shahraeini@gmail.com>
2025-06-28 20:07:51 +0330

    Base app info

--- info/admin.py
+++ info/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import AboutUs, ContactUs
+#############################################
+#              AboutUs Admin                #
+#############################################
 
-# Register your models here.
+class AboutUsAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "created_at", "updated_at"]
+    search_fields = ["title", "content"]
+    ordering = ["id"]
+    readonly_fields = ["id", "created_at", "updated_at"]
+    date_hierarchy = 'created_at'
+    list_per_page = 20
+    
+#############################################
+#              ContactUs Admin              #
+#############################################
+
+class ContactUsAdmin(admin.ModelAdmin):
+    list_display = ["id", "phone_number", "created_at", "updated_at"]
+    search_fields = ["phone_number"]
+    ordering = ["id"]
+    readonly_fields = ["id", "created_at", "updated_at"]
+    date_hierarchy = 'created_at'
+    list_per_page = 20
+    
+admin.site.register(AboutUs, AboutUsAdmin)
+admin.site.register(ContactUs, ContactUsAdmin)
