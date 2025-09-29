10905424c4a38d83f47824460e7781a2f3080f9d
Behnoush <behnoush.shahraeini@gmail.com>
2025-09-29 19:53:03 +0330

    Update utility app

--- utility/mixins.py
+++ utility/mixins.py
@@ -1,9 +1,13 @@
 from rest_framework import status
 from rest_framework.response import Response
 
-
 class SoftDeleteMixin:
     def perform_destroy(self, instance):
         instance.is_deleted = True
         instance.save()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+class RestoreMixin:
+    def perform_restore(self, instance):
+        instance.restore()
+        return Response(status=status.HTTP_200_OK)
